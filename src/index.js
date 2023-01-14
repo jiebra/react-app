22b7b38e15ce27cb036898060c47dd37166e524f
unknown <eliudgatiki9@gmail.com>
2023-01-14 16:38:38 +0300

    hello- react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Sex from './test';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Sex jina="jiebran" />
+  </React.StrictMode>
+);
+
+
+
---
+++ src/test.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from 'react';
+
+function Sex (props){
+    
+const [Count,setCount]=useState(0);
+function increase(){
+setCount(Count +1)
+}
+function decrease(){
+    setCount(Count -1)
+    }
+function reset(){
+    setCount(Count*0)
+}
+    return(
+        <div> 
+            <h1> Countering: {Count} </h1> 
+        <button onClick={decrease}>decrease</button>
+        <button onClick={increase}>increase</button>
+        <button onClick={reset}>reset</button>
+        </div>
+
+    )
+}
+export default Sex;
